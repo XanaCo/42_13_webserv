4bce17e81103de3859a6e8bd7e3b04503f2c9272
Alban TARDIF <atardif@made-f0Dr8s9.clusters.42paris.fr>
2023-11-24 21:35:36 +0100

    Nano trash serv

--- trash/main.cpp
+++ trash/main.cpp
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <netdb.h>
 #include <sys/socket.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/select.h>
 #include <sys/types.h>
@@ -15,6 +16,7 @@
 #include <sys/stat.h>
 #include <signal.h>
 #include <unistd.h>
+#include <cstdio>
 
 // We'll try to implement a first baby server, in which we'll try to put in application all the documentation we've read in order to take a grasp of how a server works, TCP protocol and maybe HTTP protocol
 
@@ -26,7 +28,7 @@ int main(void){
 
     if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        strerror(errno);
+        printf("Error with socket\n");
         return (1);
     }
 
@@ -42,9 +44,34 @@ int main(void){
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
-        strerror(errno);
+        printf("Error binding \n");
         return (1);
     }
+
+    while (1)
+    {
+        if (listen(server_fd, 10) < 0)
+        {
+            printf("Error listening\n");
+            return(1);
+        }
+        int new_socket;
+        size_t  addrlen = sizeof(address);
+        if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t *)&addrlen)) < 0) 
+        {
+            printf("Error accepting\n");
+            return(1);
+        }
+        char    buffer[1024] = {0};
+        int valread = read(new_socket, buffer, 1024);
+        write(1, buffer, strlen(buffer));
+        if (valread < 0)
+            write(2, "Read error\n", 11);
+        char test[] = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 23\n\nThis project seems huge";
+        write(new_socket, test, strlen(test));
+        close(new_socket);
+    }
+    close(server_fd);
     return (0);
 }
 
