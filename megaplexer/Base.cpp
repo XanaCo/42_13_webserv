66d5ea3192d08d25b32feef0bbf1359121846f54
Alban TARDIF <atardif@made-f0Dr8s8.clusters.42paris.fr>
2023-12-07 15:45:37 +0100

    privatized baby

--- megaplexer/Base.cpp
+++ megaplexer/Base.cpp
@@ -2,16 +2,16 @@
 
 // Constructors
 
-Base::Base() : servers(0), clients(0), pfds(0), sock_count(0){ 
+Base::Base() : _servers(0), _clients(0), _pfds(0), _sock_count(0){ 
     return ;
 }
 
-Base::Base(int argc, char **argv) : clients(0), sock_count(0){
+Base::Base(int argc, char **argv) : _clients(0), _sock_count(0){
     for (int i = 0; i + 1 < argc; i++)
     {
         this->add_to_servers(argv[i + 1]);
-        this->add_to_poll_in(servers[i].listen_socket);
-        std::cout << servers[i];
+        this->add_to_poll_in(_servers[i].listen_socket);
+        std::cout << _servers[i];
     }
     std::cout << std::endl << std::endl;
     return ;
@@ -20,7 +20,6 @@ Base::Base(int argc, char **argv) : clients(0), sock_count(0){
 Base::Base(const Base & rhs) {
     *this = rhs; 
 }
-
 
 // Destructors
 
@@ -31,14 +30,13 @@ Base::~Base(){
 // Operator overload
 
 Base &  Base::operator=(const Base & rhs) { 
-    this->servers = rhs.servers; 
-    this->clients = rhs.clients; 
-    this->pfds = rhs.pfds; 
+    this->_servers = rhs._servers; 
+    this->_clients = rhs._clients; 
+    this->_pfds = rhs._pfds; 
     return *this;
 }
 
 // Methods
-
 
 // These functions are used to add into our 3 vectors depending of a port number or socket number
 
@@ -46,14 +44,14 @@ void    Base::add_to_servers(char *port){
     
     Server  server(port);
 
-    this->servers.push_back(server);
+    this->_servers.push_back(server);
 }
 
 void    Base::add_to_clients(int socket, struct sockaddr_in* address){
 
     Client  client(socket, address);
 
-    this->clients.push_back(client);
+    this->_clients.push_back(client);
 }
 
 void    Base::add_to_poll_in(int socket)
@@ -63,8 +61,8 @@ void    Base::add_to_poll_in(int socket)
     pfd.fd = socket;
     pfd.events = POLLIN;
     pfd.revents = 0;
-    this->pfds.push_back(pfd);
-    this->sock_count++;
+    this->_pfds.push_back(pfd);
+    this->_sock_count++;
 }
 
 void    Base::add_to_poll_out(int socket)
@@ -74,21 +72,20 @@ void    Base::add_to_poll_out(int socket)
     pfd.fd = socket;
     pfd.events = POLLOUT;
     pfd.revents = 0;
-    this->pfds.push_back(pfd);
-    this->sock_count++;
+    this->_pfds.push_back(pfd);
+    this->_sock_count++;
 }
-
 
 // These functions are used to remove items from our 3 vectors
 
 void    Base::remove_from_servers(int socket){
 
-    for(unsigned int i = 0; i < this->servers.size() ; i++)
+    for(unsigned int i = 0; i < this->_servers.size() ; i++)
     {
-        if (socket == servers[i].listen_socket)
+        if (socket == _servers[i].listen_socket)
         {
             //std::cout << "Server n " << socket << " trying to be erased" << std::endl;
-            servers.erase(servers.begin() + i);
+            _servers.erase(_servers.begin() + i);
             //std::cout << "Server n " << socket << " erased" << std::endl;
             break ;
         }
@@ -97,12 +94,12 @@ void    Base::remove_from_servers(int socket){
 
 void    Base::remove_from_clients(int socket){
 
-    for(unsigned int i = 0; i < this->clients.size() ; i++)
+    for(unsigned int i = 0; i < this->_clients.size() ; i++)
     {
-        if (socket == clients[i].new_socket)
+        if (socket == _clients[i].new_socket)
         {
             //std::cout << "Client n " << socket << " trying to be erased" << std::endl;
-            clients.erase(clients.begin() + i);
+            _clients.erase(_clients.begin() + i);
             //std::cout << "Client n " << socket << " erased" << std::endl;
             break ;
         }
@@ -111,49 +108,18 @@ void    Base::remove_from_clients(int socket){
 
 void    Base::remove_from_poll(int socket){
 
-    for(unsigned int i = 0; i < this->pfds.size() ; i++)
+    for(unsigned int i = 0; i < this->_pfds.size() ; i++)
     {
-        if (socket == pfds[i].fd)
+        if (socket == _pfds[i].fd)
         {
             //std::cout << "fd n " << socket << " trying to be erased from poll" << std::endl;
-            close(pfds[i].fd);
-            pfds.erase(pfds.begin() + i);
-            this->sock_count--;
+            close(_pfds[i].fd);
+            _pfds.erase(_pfds.begin() + i);
+            this->_sock_count--;
             //std::cout << "fd n " << socket << " erased from poll" << std::endl;
             break ;
         }
     }
-}
-
-// This function is used to identify if a given fd is a listening fd
-
-bool    Base::is_a_server(int socket)
-{
-    for (unsigned int i = 0; i < this->servers.size(); i++)
-    {
-        if (servers[i].listen_socket == socket)
-            return true;
-    }
-    return false;
-}
-
-// These functions are used to return sockaddr_in or sockaddr_in->sin_addr
-
-void*   Base::get_in_addr(struct sockaddr *sa)
-{
-    if (sa->sa_family == AF_INET){
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
-void*   Base::get_in_sockaddr(struct sockaddr *sa){
-
-    if (sa->sa_family == AF_INET){
-        return (((struct sockaddr_in*)sa));
-    }
-    return (((struct sockaddr_in6*)sa));
-
 }
 
 // function used to accept new connection on listening socket
@@ -175,32 +141,6 @@ void    Base::handle_new_connection(int serv_sock)
         std::cout << "New connection from " << inet_ntop(remoteaddr.ss_family, get_in_addr((struct sockaddr*)&remoteaddr), remoteIP, INET6_ADDRSTRLEN) << " on socket n " << new_fd << std::endl; 
         this->add_to_clients(new_fd, (struct sockaddr_in*)this->get_in_sockaddr((struct sockaddr*)&remoteaddr));
     }
-}
-
-// function used to return a client/poll ref from its socket
-
-Client &    Base::get_cli_from_sock(int client_sock){
-
-    unsigned int i = 0;
-    while (i < this->clients.size())
-    {
-        if (clients[i].new_socket == client_sock)
-            break ;
-        i++;
-    }
-    return clients[i];
-}
-
-struct pollfd    *Base::get_poll_from_sock(int client_sock){
-
-    unsigned int i = 0;
-    while (i < this->pfds.size())
-    {
-        if (pfds[i].fd == client_sock)
-            break ;
-        i++;
-    }
-    return (&pfds[i]);
 }
 
 // function used to change a poll event
@@ -245,29 +185,86 @@ void    Base::receive_client_data(int client_sock){
     std::cout << client.received << std::endl;
 }
 
-// function used to review the poll of fd
+// These functions are used to return sockaddr_in or sockaddr_in->sin_addr
 
-void    Base::review_poll(void){
-
-    char test[] = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 45\n\nPablo va bientot m'envoyer de belles reponses";
-
-    for (int i = 0; i < this->sock_count; i++)
-    {
-        if (pfds[i].revents & POLLIN)
-        {
-            if (is_a_server(pfds[i].fd))
-                handle_new_connection(pfds[i].fd);
-            else
-                receive_client_data(pfds[i].fd);
-        }
-        if(pfds[i].revents & POLLOUT)
-        {
-            int b_send;
-
-            b_send = send(pfds[i].fd, test, strlen(test), 0);
-            change_poll_event(pfds[i].fd, pollin);
-        }
+void*   Base::get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET){
+        return &(((struct sockaddr_in*)sa)->sin_addr);
     }
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+void*   Base::get_in_sockaddr(struct sockaddr *sa){
+
+    if (sa->sa_family == AF_INET){
+        return (((struct sockaddr_in*)sa));
+    }
+    return (((struct sockaddr_in6*)sa));
+
+}
+
+// This function is used to identify if a given fd is a listening fd
+
+bool    Base::is_a_server(int socket)
+{
+    for (unsigned int i = 0; i < this->_servers.size(); i++)
+    {
+        if (_servers[i].listen_socket == socket)
+            return true;
+    }
+    return false;
+}
+
+// This function is used to identify if a given fd is a client fd
+
+bool    Base::is_a_client(int socket)
+{
+    for (unsigned int i = 0; i < this->_clients.size(); i++)
+    {
+        if (_clients[i].new_socket == socket)
+            return true;
+    }
+    return false;
+}
+
+// function used to return a client/poll ref from its socket
+
+Client &    Base::get_cli_from_sock(int client_sock){
+
+    unsigned int i = 0;
+    while (i < this->_clients.size())
+    {
+        if (_clients[i].new_socket == client_sock)
+            break ;
+        i++;
+    }
+    return _clients[i];
+}
+
+Server &    Base::get_serv_from_sock(int sock){
+
+    unsigned int i = 0;
+    while (i < this->_servers.size())
+    {
+        if (_servers[i].listen_socket == sock)
+            break ;
+        i++;
+    }
+    return _servers[i];
+}
+
+
+struct pollfd    *Base::get_poll_from_sock(int client_sock){
+
+    unsigned int i = 0;
+    while (i < this->_pfds.size())
+    {
+        if (_pfds[i].fd == client_sock)
+            break ;
+        i++;
+    }
+    return (&_pfds[i]);
 }
 
 // main loop that only call the poll reviewer
@@ -276,12 +273,37 @@ void    Base::start_servers(void) {
 
     while (1)
     {
-       int  poll_count = poll(this->pfds.data(), this->pfds.size(), -1); 
+       int  poll_count = poll(this->_pfds.data(), this->_pfds.size(), -1); 
        if (poll_count == -1)
        {
            std::cout << "Error in polling sockets" << std::endl;
            break ;
        }
        this->review_poll();
+    }
+}
+
+// function used to review the poll of fd
+
+void    Base::review_poll(void){
+
+    char test[] = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 45\n\nPablo va bientot m'envoyer de belles reponses";
+
+    for (int i = 0; i < this->_sock_count; i++)
+    {
+        if (_pfds[i].revents & POLLIN)
+        {
+            if (is_a_server(_pfds[i].fd))
+                handle_new_connection(_pfds[i].fd);
+            else
+                receive_client_data(_pfds[i].fd);
+        }
+        if(_pfds[i].revents & POLLOUT)
+        {
+            int b_send;
+
+            b_send = send(_pfds[i].fd, test, strlen(test), 0);
+            change_poll_event(_pfds[i].fd, pollin);
+        }
     }
 }
